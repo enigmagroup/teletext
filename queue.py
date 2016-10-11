f1b8ad4fab3d923c5485ea2b815697f86bcd7953
Agent 42 <42@enigmabox.net>
2016-10-11 18:48:56 +0200

    queue cleanup imports

--- queue.py
+++ queue.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python
 
 import beanstalkc
-import sqlite3
-from gevent import spawn, sleep, monkey; monkey.patch_all()
-from bottle import route, error, run, static_file, template, request, abort, redirect, debug, default_app, html_escape
-from urllib import quote
-from urllib2 import urlopen
-from datetime import datetime, timedelta
-from time import timezone, strptime, strftime, mktime
-from json import loads as json_loads, dumps as json_dumps
-from re import compile as re_compile
 
 ################################################################################
 # queue class
@@ -29,4 +20,3 @@ class Queue():
 
     def close(self):
         self.bs.close()
-
